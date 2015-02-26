2c58d7a3c27205c2e2ab789e0990cd6b3503efcb
Juan Oliva <alu0100502923@ull.edu.es>
2015-02-26 10:30:01 +0000

    Añadiendo Pruebas

--- tests/tests.js
+++ tests/tests.js
@@ -1,19 +1,29 @@
 var assert = chai.assert;
 
-suite('temperature', function() {
-    test('32F = 0.0C', function() {
-        original.value = "32F";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "0.0 Celcius");
-    });
-    test('45C = 113.0 Farenheit', function() {
-        original.value = "45C";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
-    });
-    test('5X = error', function() {
-        original.value = "5X";
-        calculate();
-        assert.match(converted.innerHTML, /ERROR/);
-    });
+suite('Temperature Converter Default Test:', function() {
+  test('is 32F equal to 0C ?', function() {
+    original.value = "32F";
+    calculate();
+    assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+  });
+  test('is 45C equal to 113.0 Farenheit?', function() {
+    original.value = "45C";
+    calculate();
+    assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+  });
+  test('is 28 C equal to 82.4 Farenheit?', function() {
+    original.value = "  28  C  ";
+    calculate();
+    assert.deepEqual(converted.innerHTML, "82.4 Farenheit");
+  });
+  test('is +65.8C equal to 154.4 Farenheit?', function() {
+    original.value = "+65.8C";
+    calculate();
+    assert.deepEqual(converted.innerHTML, "150.4 Farenheit");
+  });
+  test('is +  65.8 C equal to 154.4 Farenheit?', function() {
+    original.value = "+65.8 C ";
+    calculate();
+    assert.deepEqual(converted.innerHTML, "150.4 Farenheit");
+  });
 });
